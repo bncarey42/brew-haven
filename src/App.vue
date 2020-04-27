48d0ba698fa617dd21a7ab0deeeada7e4a697c58
ben <carey.benjamin42@gmail.com>
2020-04-26 21:33:45 -0500

    get basic location data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
     <div id="app" class="font-family-main container-fluid">
+        <loading/>
         <HeaderBar>
             <b-btn v-if="breweries.length > 0"
                    @click="resetList()"
                    class="float-right bg-yellow text-dark-blue font-weight-bold border-0">Reset Brewer List
             </b-btn>
         </HeaderBar>
-        <loading/>
+        <div class="row">
+            <div class="col-12">
+                <div v-if="gettingLocation">
+                    <i>Getting your location...</i>
+                </div>
+
+                <div v-if="location_data">
+                    Your device location is {{location_data.coords.latitude}} / {{location_data.coords.longitude}}
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-10 offset-1">
                 <div v-if="breweries.length > 0">
@@ -15,8 +26,6 @@
                 <BrewMap v-else/>
             </div>
         </div>
-
-
     </div>
 </template>
 
@@ -31,7 +40,9 @@
                 breweries: [],
                 searchType: null,
                 searchVal: null,
-                more: true
+                more: true,
+                location_data : null,
+                gettingLocation: false,
             }
         },
         created() {
@@ -42,17 +53,34 @@
             });
 
             EB.$on('reset-brewer-list', () => this.reset());
+
+            EB.$on('set-location', () => {
+                if (!("geolocation" in navigator)) {
+                    this.errorStr = 'Geolocation is not available.';
+                    return;
+                }
+                this.gettingLocation = true;
+                navigator.geolocation.getCurrentPosition(position => {
+                    this.gettingLocation = false;
+                    this.location_data = position;
+                }, error => {
+                    this.gettingLocation = false;
+                    console.log(error.message);
+                });
+            });
         },
         methods: {
             resetList: function () {
                 EB.$emit('reset-brewer-list');
             },
+
             reset: function () {
                 this.breweries = [];
                 this.searchType = null;
                 this.searchVal = null;
                 this.more = true;
             },
+
             loadBrews: function (page = 1, perPage = 25) {
                 EB.$emit('toggle-loading', `loading breweries for ${this.searchVal}`);
                 fetch(`${this.baseBreweryURL}?by_${this.searchType}=${this.searchVal}&per_page=${perPage}&page=${page}&sort=name`, {
@@ -72,7 +100,7 @@
                         console.log(err);
                         EB.$emit('toggle-loading');
                     });
-            },
+            }
         }
     }
 </script>
--- src/components/top/headerbar.vue
+++ src/components/top/headerbar.vue
@@ -1,11 +1,12 @@
 <template>
     <nav class="w-100 p-1 shadow-sm bg-dark-blue mb-4">
         <div class="row">
-            <div class="col-3"><h3 class="font-family-header text-yellow">BREWHAVEN!</h3></div>
+            <div class="col-3"><h3 class="font-family-header text-yellow">Brew House</h3></div>
             <div class="col-6">
                 <div class="row">
                     <div class="col-2">
-                        <b-select :options="searchOptions" v-model="search" placeholder="'Search By'" class="text-dark-blue"/>
+                        <b-select :options="searchOptions" v-model="search" placeholder="'Search By'"
+                                  class="text-dark-blue"/>
                     </div>
                     <div class="col-8">
                         <div class="input-group border-0 h-100 w-100 bg-white rounded">
@@ -15,15 +16,25 @@
                                 </span>
                             </div>
                             <input type="text"
-                                   placeholder="Input your text here or click a state below"
+                                   placeholder="Input your search here or click a state below"
                                    class="border-0 rounded-right bg-white float-right w-75"
                                    aria-describedby="search-addon"
                                    v-model="searchTerms"
+                                   v-if="search !== 'Locations'"
+                            >
+                            <input type="number"
+                                   placeholder="Input the number of miles from your current location you'd like to search"
+                                   class="border-0 rounded-right bg-white float-right w-75"
+                                   aria-describedby="search-addon"
+                                   v-model="searchTerms"
+                                   v-else
                             >
                         </div>
                     </div>
                     <div class="col-2">
-                        <b-btn class="search-btn bg-yellow text-dark-blue font-weight-bold border-0" @click="search_by()" block>SEARCH</b-btn>
+                        <b-btn class="search-btn bg-yellow text-dark-blue font-weight-bold border-0"
+                               @click="search_by()" block>SEARCH
+                        </b-btn>
                     </div>
                 </div>
             </div>
@@ -42,12 +53,19 @@
         data: function () {
             return {
                 search: 'State',
-                searchOptions: ['City', 'State', 'Name', 'Zip', 'Tags'],
+                searchOptions: ['City', 'State', 'Name', 'Zip', 'Locations', 'Tags'],
                 searchTerms: null,
             }
         },
-        methods:{
-            search_by: function() {
+        watch: {
+            search: function (search_val) {
+                if (search_val === 'Locations') {
+                    EB.$emit('set-location');
+                }
+            }
+        },
+        methods: {
+            search_by: function () {
                 this.reset();
                 EB.$emit('load-breweries', {
                     type: this.search.toLowerCase(),
@@ -55,7 +73,7 @@
                     page: 0
                 })
             },
-            reset: function() {
+            reset: function () {
                 EB.$emit('reset-brewer-list');
             }
         }
